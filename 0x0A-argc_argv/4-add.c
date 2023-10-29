ab97ad180dbbcbaca71141615f3662d6a107aea2
Rev-Allen <profallenyates@gmail.com>
2023-10-29 14:57:48 -0700

    C - argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,63 +1,36 @@
 #include <stdio.h>
-	#include <stdlib.h>
-	#include <ctype.h>
-	#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-	/**
-	 * check_num - check string there are digit
-	 * @str: array str
-	 *
-	 * Return: Always 0 (Success)
-	 */
-	int check_num(char *str)
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int sum = 0;
+
+	if (argc == 1)
+		printf("%d\n", 0);
+	else
 	{
-		/*Declaring variables*/
-		unsigned int count;
-
-		count = 0;
-		while (count < strlen(str)) /*count string*/
+		for (i = 1; i < argc; i++)
 		{
-			if (!isdigit(str[count])) /*check if str there are digit*/
+			for (j = 0; argv[i][j] != '\0' ; j++)
 			{
-				return (0);
+				if (!(isdigit(argv[i][j])))
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			count++;
+			sum = sum + atol(argv[i]);
 		}
-		return (1);
+		printf("%d\n", sum);
 	}
-
-
-	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
-
-	int main(int argc, char *argv[])
-	{
-		/*Declaring variables*/
-		int count;
-		int str_to_int;
-		int sum = 0;
-
-		count = 1;
-		while (count < argc) /*Goes through the whole array*/
-		{
-			if (check_num(argv[count]))
-			{
-				str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-				sum += str_to_int;
-			}
-			/*Condition if one of the number contains symbols that are not digits*/
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-			count++;
-		}
-		printf("%d\n", sum); /*print sum*/
-		return (0);
-	}
+	return (0);
+}

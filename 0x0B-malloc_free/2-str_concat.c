3b15cfda5558c3e9643076dd16c48f873e3c5258
Rev-Allen <profallenyates@gmail.com>
2023-10-30 14:10:36 -0700

    C - malloc, free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,7 +30,7 @@ char *str_concat(char *s1, char *s2)
 	if (output == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
-		output[i] = s1[1];
+		output[i] = s1[i];
 	for (i = 0; s2[i] != '\0'; i++)
 		output[s1len + i] = s2[i];
 	return (output);
